68b9f3df1816e1d1f706699ec431362f79ae0dd7
p.gharibi <p.gharibi@etick.ir>
2019-05-15 09:22:39 +0430

    reset data in test

--- pasha-grpc-wallet-cache/src/test/java/pasha/grpc/wallet/practical/client/WalletClientTest.java
+++ pasha-grpc-wallet-cache/src/test/java/pasha/grpc/wallet/practical/client/WalletClientTest.java
@@ -1,13 +1,18 @@
 package pasha.grpc.wallet.practical.client;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import pasha.grpc.wallet.cache.DemoApp;
 import pasha.grpc.wallet.cache.client.WalletClient;
+import pasha.grpc.wallet.cache.server.repo.FundRepository;
 import pasha.grpc.wallet.practical.server.GrpcServerTestBase;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by p.gharibi on 5/11/2019 at 15.
@@ -18,6 +23,27 @@ public class WalletClientTest extends GrpcServerTestBase {
 
     @Autowired
     WalletClient walletClient;
+
+    @Autowired
+    FundRepository fundRepository;
+
+
+
+    @Autowired
+    @Qualifier("fundCache")
+    ConcurrentHashMap cache;
+
+    @Before
+    public final void resetData() {
+        fundRepository.deleteAll();
+        cache.clear();
+        System.out.println("All data truncated!");
+
+        walletClient.setNumberOfCuncurrentUsers(10);
+        walletClient.setConcurrentThreadsPerUser(11);
+        walletClient.setRoundsPerThread(12);
+
+    }
 
     @Test
     public void testClient() throws InterruptedException {
